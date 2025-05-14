05f1eddaf212c94a6473598687c7f232d575325c
Matthew O'Brien <122697844+MatthewKonobri@users.noreply.github.com>
2025-05-14 15:31:24 -0400

    Removed resource _inti functions

--- addons/tile_to_gridmap/resources/t2g_prop.gd
+++ addons/tile_to_gridmap/resources/t2g_prop.gd
@@ -5,11 +5,3 @@ class_name T2GProp
 @export var tiles : Array[String]
 @export_range (0 , 1) var chance : float
 @export var scene : PackedScene
-
-func _init(
-			init_name = "",
-			init_tile = [], 
-			init_chance = 0, 
-			init_scene = ""):
-	name = init_name
-	chance = init_chance
--- addons/tile_to_gridmap/resources/t2g_terrain.gd
+++ addons/tile_to_gridmap/resources/t2g_terrain.gd
@@ -7,17 +7,3 @@ class_name T2GTerrain
 @export var atlas_coordinates : Vector2i
 @export var transition_tile_outer : Vector2i
 @export var transition_tile_inner : Vector2i
-
-func _init(
-			init_name = [],
-			init_noise_min = 0, 
-			init_noise_max = 0, 
-			init_atlas_coordinates = (0), 
-			init_transition_tile_outer = (0), 
-			init_transition_tile_inner = (0)):
-	name = init_name
-	noise_min = init_noise_min
-	noise_max = init_noise_max
-	atlas_coordinates = init_atlas_coordinates
-	transition_tile_outer = init_transition_tile_inner
-	transition_tile_outer = init_transition_tile_outer
